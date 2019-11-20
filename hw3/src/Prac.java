4e3bb80449a1ed2458914f31b1971486807e5248
dreamsh19 <dreamsh19@snu.ac.kr>
2019-11-20 21:50:34 +0900

    Implement Solution2

---
+++ hw3/src/Prac.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Prac {
+    public static void main(String[] args){
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
--- hw3/src/Solution2.java
+++ hw3/src/Solution2.java
@@ -21,10 +21,175 @@ import java.io.PrintWriter;
 class Solution2 {
 	static final int MAX_N = 20000;
 	static final int MAX_E = 80000;
-
+	static final int LEFT = 0;
+	static final int RIGHT =1;
+	static final int ROOT =1;
+	static final int START = 1;
+	static final int WEIGHT_MIN = -1;
 	static int N, E;
 	static int[] U = new int[MAX_E], V = new int[MAX_E], W = new int[MAX_E];
 	static int Answer;
+	static Vertex[] Vertices = new Vertex[MAX_N+1];
+
+	static void prim(){
+		Answer=0;
+		heapNode[] init = buildInitialHeap();
+		maxHeap maxHeap = new maxHeap(init);
+
+		maxHeap.update(START,0);
+		while(!maxHeap.isEmpty()){
+			heapNode maxNode = maxHeap.extractMax();
+//			System.out.printf("VID[%d]KEY[%d]\n",maxNode.vertexID,maxNode.key);
+			Answer+=maxNode.key;
+			Vertices[maxNode.vertexID].inMST=true;
+			adjListNode adjListNode=Vertices[maxNode.vertexID].head;
+			while(adjListNode!=null){
+				int VID=adjListNode.vertexID;
+				Vertex v=Vertices[VID];
+				if(!v.inMST && maxHeap.heap[maxHeap.indexOf[VID]].key < adjListNode.weight){
+					maxHeap.update(VID,adjListNode.weight);
+				}
+				adjListNode=adjListNode.next;
+			}
+//			maxHeap.printHeap();
+//			break;
+		}
+
+
+	}
+	static heapNode[] buildInitialHeap(){
+		heapNode[] init = new heapNode[N+1];
+		for(int i=1;i<=N;i++){
+			init[i]=new heapNode(i,WEIGHT_MIN);
+		}
+		return init;
+	}
+	static class maxHeap{
+		private heapNode[] heap;
+		int len;
+		int[] indexOf;
+
+
+		maxHeap(heapNode[] init){
+			heap = init;
+			len = init.length-1;
+			indexOf = new int[len+1];
+			for(int i=1;i<=len;i++){
+				indexOf[heap[i].vertexID]=i;
+			}
+		}
+		boolean isEmpty(){
+			return len==0;
+		}
+		int parent(int i){
+			return i/2;
+		}
+		int child(int i, int is_right){
+			return 2*i + is_right;
+		}
+
+		void printHeap(){
+			String s="===========printHeap() start=============\n";
+			for(int i=1;i<=len;i++){
+				s+=String.format("IDX[%d]VID[%d]KEY[%d]\n",i,heap[i].vertexID,heap[i].key);
+			}
+			s+="===========printHeap() end==============\n";
+			System.out.println(s);
+
+		}
+		void swap(int parent, int child){
+
+			heapNode childNode = heap[child];
+			heapNode parentNode = heap[parent];
+			indexOf[childNode.vertexID] = parent;
+			indexOf[parentNode.vertexID] = child;
+			heap[child]=parentNode;
+			heap[parent]=childNode;
+
+		}
+		boolean hasParent(int i){
+			return i>1;
+		}
+
+		void update(int id,int newKey){
+			int heapIdx= indexOf[id];
+			heapNode node = heap[heapIdx];
+			node.key=newKey;
+			bubbleUp(heapIdx);
+		}
+		void bubbleUp(int i){
+			if(!hasParent(i)) return;
+
+			heapNode child = heap[i];
+			heapNode parent = heap[parent(i)];
+
+			if(child.compareTo(parent)>0){
+				swap(parent(i),i);
+				bubbleUp(parent(i));
+			}
+		}
+		void bubbleDown(int i){
+			int maxIdx = i;
+			int leftIdx = child(i,LEFT);
+			int rightIdx = child(i,RIGHT);
+
+			if(leftIdx <= len && heap[leftIdx].compareTo(heap[maxIdx])>0){
+				maxIdx = leftIdx;
+			}
+			if(rightIdx <= len && heap[rightIdx].compareTo(heap[maxIdx])>0){
+				maxIdx = rightIdx;
+			}
+			if(maxIdx!=i){
+				swap(i,maxIdx);
+				bubbleDown(maxIdx);
+			}
+
+		}
+		heapNode extractMax(){
+			heapNode maxNode=heap[ROOT];
+			swap(ROOT,len--);
+			bubbleDown(ROOT);
+			return maxNode;
+		}
+	}
+
+	static class adjListNode{
+		int vertexID;
+		int weight;
+		adjListNode next;
+
+		adjListNode(int vertexID, int weight){
+			this.vertexID=vertexID;
+			this.weight=weight;
+			this.next=null;
+		}
+	}
+	static class Vertex{
+		adjListNode head;
+		boolean inMST;
+
+		Vertex(){
+			this(null);
+		}
+		Vertex(adjListNode head){
+			this.head=head;
+			inMST=false;
+		}
+	}
+	static class heapNode implements Comparable<heapNode>{
+
+		int vertexID;
+		int key;
+
+		heapNode(int vertexID,int key){
+			this.vertexID=vertexID;
+			this.key=key;
+		}
+		@Override
+		public int compareTo(heapNode o) {
+			return this.key-o.key;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		/*
@@ -53,15 +218,40 @@ class Solution2 {
 				W[i] = Integer.parseInt(stk.nextToken());
 			}
 
+			for(int i=1;i<=N;i++){
+				Vertices[i]=new Vertex();
+			}
 
+			for( int i=0;i<E;i++){
+				Vertex u = Vertices[U[i]];
+				Vertex v = Vertices[V[i]];
+				int weight = W[i];
+
+				adjListNode node_v=new adjListNode(V[i],weight);
+				adjListNode node_u=new adjListNode(U[i],weight);
+
+				node_v.next = u.head;
+				u.head = node_v;
+				node_u.next = v.head;
+				v.head = node_u;
+			}
+
+//			for(int i=1;i<=N;i++){
+//				Vertex v = Vertices[i];
+//				System.out.printf("\nvertex[%d]",i);
+//				adjListNode head= v.head;
+//				while(head!=null){
+//					System.out.printf("[%d-%d]",head.vertexID,head.weight);
+//					head=head.next;
+//				}
+//			}
 			/////////////////////////////////////////////////////////////////////////////////////////////
 			/*
 			   이 부분에서 여러분의 알고리즘이 수행됩니다.
 			   문제의 답을 계산하여 그 값을 Answer에 저장하는 것을 가정하였습니다.
 			 */
 			/////////////////////////////////////////////////////////////////////////////////////////////
-			Answer = 0;
-
+			prim();
 
 			// output2.txt로 답안을 출력합니다.
 			pw.println("#" + test_case + " " + Answer);
